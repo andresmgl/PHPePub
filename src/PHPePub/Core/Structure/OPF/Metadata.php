9bb958ad8323f7537904bb37acc93e924bbfe84c
andresmgl <78852789+andresmgl@users.noreply.github.com>
2024-11-28 09:35:26 +0100

    Update Metadata.php
    PHP compatibility bug fixes

--- src/PHPePub/Core/Structure/OPF/Metadata.php
+++ src/PHPePub/Core/Structure/OPF/Metadata.php
@@ -123,15 +123,21 @@ class Metadata {
         }
 
         foreach ($this->metaProperties as $data) {
-            list($name, $content) = each($data);
+            // list($name, $content) = each($data);
+            foreach ($data as $name => $content) {
+                break;
+            }
             $metadata .= "\t\t<meta property=\"" . $name . "\">" . $content . "</meta>\n";
         }
 
         foreach ($this->meta as $data) {
-            list($name, $content) = each($data);
+            // list($name, $content) = each($data);
+            foreach ($data as $name => $content) {
+                break;
+            }
             $metadata .= "\t\t<meta name=\"" . $name . "\" content=\"" . $content . "\" />\n";
         }
-
+        
         return $metadata . "\t</metadata>\n";
     }
 }
